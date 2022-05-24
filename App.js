da0c872bb620584492b4bbaa8fe090cad36da51c
wanessaramos <wanessinha_ramos@yahoo.com.br>
2022-05-24 17:50:55 -0300

    add new App

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useReducer } from 'react';
+import Context from './src/context';
+import Routes from './src/routes';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost';
+
+const DATA = []
+
+const initialState = {DATA};
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  function reducer(state,action){
+
+    if(action.type === 'ADD'){
+
+      const newItem = action.payload
+  
+      return{
+        ...state,
+        DATA: [...state.DATA, newItem],
+      }
+
+    }else if(action.type === 'UPDATE'){
+
+      const updateItem = action.payload
+
+      return{
+        ...state,
+        DATA: state.DATA.map(item => item.id === updateItem.id ? updateItem : item),
+      }
+
+    }else{
+
+      return{
+        ...state,
+        DATA: state.DATA.filter(item => item.id !== itemId),
+      }
+
+    }
+   
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  if(!fontsLoaded){
+    return(
+      <AppLoading/>
+    ) 
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Context.Provider value={{state,dispatch}}>
+        <Routes/>
+    </Context.Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
